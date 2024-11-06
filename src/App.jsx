d61d244a3b5b211ca247510f4c0c642cb03d97ba
olamide olayemi <olamid15@gmail.com>
2024-11-06 17:38:13 +0100

    Added mouse move event listener and cursor glow effect to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import { Route, Routes } from "react-router-dom"
-import Home from "./components/Home"
-import About from "./components/About"
-import Projects from "./components/Projects"
-import Contact from "./components/Contact"
-import Header from "./components/header/Header"
-import Page404 from "./components/pages/Page404"
-import Footer from "./components/footer/Footer"
-import { ThemeProvider } from "./components/themeContext/ThemeContext"
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Header from "./components/header/Header";
+import Page404 from "./components/pages/Page404";
+import Footer from "./components/footer/Footer";
+import { ThemeProvider } from "./components/themeContext/ThemeContext";
+import { useState, useEffect } from "react";
+import "./App.css"; // Import the custom CSS file
 
 function App() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setPosition({ x: event.clientX, y: event.clientY });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
 
   return (
     <>
@@ -24,9 +38,18 @@ function App() {
           </Routes>
         </div>
         <Footer />
+
+        {/* Glow Effect */}
+        <div
+          className="cursor-glow"
+          style={{
+            left: `${position.x}px`,
+            top: `${position.y}px`,
+          }}
+        ></div>
       </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
